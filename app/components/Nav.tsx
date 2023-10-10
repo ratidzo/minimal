db6d3916eecb72f33eda87b4f7ba566900452bc6
ratidzo <ratygore@gmail.com>
2023-10-10 18:56:35 +0200

    setup nav links

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,17 +6,47 @@ import Link from "next/link"
 
 export default function Nav() {
 
-
+    const links = [
+        {
+            name: 'Product',
+            url: '/'
+        },
+        {
+            name: 'Features',
+            url: '/'
+        },
+        {
+            name: 'Markeplace',
+            url: '/'
+        },
+        {
+            name: 'Company',
+            url: '/'
+        }
+    ]
 
     return (
         <div>
+            {/* Logo container section */}
             <div>
                 <Image alt="Ionic Logo" src={'/logo-ionic.svg'} 
                  width={'40'} height={'40'} />
             </div>
 
+            {/* Nav links section. */}
             <div>
-                
+                <ul>
+                   {
+                    links.map(link => (
+                        <li>
+                            <Link href={link.url}>
+                                {link.name}
+                            </Link>
+                        </li>
+                    ))
+                   }
+                </ul>
+
             </div>
         </div>
     )
