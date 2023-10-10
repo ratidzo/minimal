86c136ac78bc81298d918d528bf6ce1a7314e3b7
ratidzo <ratygore@gmail.com>
2023-10-10 18:33:06 +0200

    add ionic logo to nav bar

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,7 +1,23 @@
+'use client'
+
+import Image from "next/image"
+import Link from "next/link"
+
 
 export default function Nav() {
+
+
+
     return (
-        <>
-        </>
+        <div>
+            <div>
+                <Image alt="Ionic Logo" src={'/logo-ionic.svg'} 
+                 width={'40'} height={'40'} />
+            </div>
+
+            <div>
+                
+            </div>
+        </div>
     )
 }
